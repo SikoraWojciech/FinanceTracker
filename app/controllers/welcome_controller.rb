277ba3df0138f1a8573ee93f908ea0cc2bb6f5c6
Wojciech Sikora <wsikora225@gmail.com>
2020-06-06 00:29:19 +0200

    Styling navigation bar and device templates

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,5 +2,8 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    if current_user
+      redirect_to my_portfolio_path
+    end
   end
 end

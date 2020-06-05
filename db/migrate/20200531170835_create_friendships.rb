0cae024768d6004521f393e53dfa1a8f9c282742
Wojciech Sikora <wsikora225@gmail.com>
2020-06-05 21:30:32 +0200

    Followers feature

--- db/migrate/20200531170835_create_friendships.rb
+++ db/migrate/20200531170835_create_friendships.rb
@@ -1,9 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration[6.0]
   def change
     create_table :friendships do |t|
-      t.string :user
-      t.string :references
-
+      t.references :user, null: false, foreign_key: true
+      t.references :friend, references: :users, foreign_key: { to_table: :users }
       t.timestamps
     end
   end
